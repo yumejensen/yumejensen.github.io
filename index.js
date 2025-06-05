87a912ad72e1b8999d4f1690f6f87c37ae9d5510
Yume Jensen <yumejensen@gmail.com>
2025-06-05 10:47:44 -0500

    click user to see timeline works!

--- index.js
+++ index.js
@@ -1,51 +1,53 @@
 $(() => {
+  // streams.home array holds objects that look like:
+// {user: 'shawndrost', message: 'just enjoyed an entire city #sf', created_at: Wed Jun 04 2025 11:03:43 GMT-0500 (Central Daylight Time)}
+  
   const $page = $('#all-contents'); // everything in the body of twiddler.html
   const $tweetsDiv = $('<div class="tweets"></div>'); // div that holds all the tweets
   $page.append($tweetsDiv); // add div that holds tweets to the body of twiddler.html
 
-  //HEADER OF WEB-PAGE - prepend to $page
+  // HEADER OF WEB-PAGE
   const $topOfPage = $('<h1 id="header">YUME TWIDDLER PAGE!</h1>');
+    //prepend to main page
     $page.prepend($topOfPage);
 
   // BUTTON TO REFRESH TWEETS
   const $refreshButton = $('<button id="new-tweets-button">REFRESH TWEETS</button>');
     // insert button after header
     $refreshButton.insertAfter($topOfPage);
-
-// streams.home array holds objects that look like:
-// {user: 'shawndrost', message: 'just enjoyed an entire city #sf', created_at: Wed Jun 04 2025 11:03:43 GMT-0500 (Central Daylight Time)}
-  
-// FUNCTION TO ADD TWEETS ---------------------------------------------------------------------------------- 
+ 
+// FUNCTION TO ADD TWEETS ----------------------------------------------------------------------------------------------------- 
 // array param to make function re-usable
 function addNewTweets(array){ // putting map into a function
-  // map over streams.home array to modify individual tweets
+  // map over streams.home array to modify individual tweets (in reverse to display most recent first)
   const $tweets = array.toReversed().map((tweet) => {
-    // create div to hold tweets inside
+    // tweet div to hold tweet inside
     const $tweet = $('<div class="tweet"></div>');
 
-    // username class username div
-    const $username = $(`<div class="username">${tweet.user}</div>`);
-      // needs a click function to show tweet history
-    // make username into a button? 
-    const $usernameButton = $(`<button class="users-button">${$username}</button>`);
-    // when you click username, show user history
-    $usernameButton.on('click', () => {
+    //USERNAME + CLICK ON USERNAME TO SEE TIMELINE ----------------------------------
+    // username (class username) button
+    const username = tweet.user;
+    const $username = $(`<button class="username">@${username}</button>`);
+    // when usename is clicked
+    $username.on('click', () => {
+      // remove tweets already ON page currently
       $tweetsDiv.empty();
-      addNewTweets(streams.users);
-    })
+      // calls addTweets with that specific user input
+      addNewTweets(streams.users[username]);
+    }) //----------------------------------------------------------------------------
 
-    //create a tag with message inside
+    // message div (class message)
     const $message = $(`<div class="message">${tweet.message}</div>`);
 
-    //create div with time in human friendly way
+    // human friendly time div (class humanFriendlyTimestamp)
     // make human friendly time with format "x seconds ago" "x minutes ago"
     const timeAgo = moment(tweet.created_at).fromNow();
       //humanFriendlyTimestamp div
       const $timesince = $(`<div class="humanFriendlyTimestamp">(${timeAgo})</div>`);
 
-    //create div to hold timestamp inside with class timestamp
+    // timestamp div (class timestamp)
       // make current date with format June 4th 2025, 3:43:10 pm
-    const timeNow = moment().format("MMMM Do YYYY, h:mm:ss a")
+    const timeNow = moment().format("MMMM Do YYYY, h:mm:ss a");
       const $timestamp = $(`<div class="timestamp">${timeNow}</div>`);
 
     // append all necessary parts to each $tweet
@@ -58,22 +60,20 @@ function addNewTweets(array){ // putting map into a function
   });
   // append every individual tweet to the larger tweets div
   $tweetsDiv.append($tweets); 
-}; // ------------------------------------------------------------------------------------------------------
-addNewTweets(streams.home); // calling function for first time load
+}; // -----------------------------------------------------------------------------------------------------------------------
+addNewTweets(streams.home); // CALL ADD TWEETS 
 
 
-// CLICK HANDLER ON NEW TWEETS BUTTON ----------------------------------------------------------------------
+// CLICK HANDLER ON REFRESH TWEETS BUTTON -----------------------------------------------------------------------------------
 $refreshButton.on('click', () => {
   // remove tweets already ON page currently
-  //$tweetsDiv.html("");
   $tweetsDiv.empty();
   // call addNewTweets function to refresh
   addNewTweets(streams.home);
 })
-//----------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------------
 
 
-  // when showing new tweets, can remove the tweets that are already on the page and then add the tweets back
-  // look up jQuery methods that will clear an element/tag - the tag that you would clear is the $tweetsDiv
 
-});
+
+}); // don't delete this 
